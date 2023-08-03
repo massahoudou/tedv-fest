97c9afde0f1f566fc6fbf7aa9c4fc8f1efcc1161
massahoudou <9324@.Light>
2023-08-03 22:12:29 +0000

    fixing animation 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: 'export',
+  output: 'export',
     images: {
         domains: ['images.unsplash.com'],
       },
-
+      
 }
 
 module.exports = nextConfig

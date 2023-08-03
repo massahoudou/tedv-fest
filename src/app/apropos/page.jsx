7f5497af63603d16e1792fd290bd14fcd5cf78c6
massahoudou <9324@.Light>
2023-08-03 20:32:12 +0000

    fixed latence adding section contact

--- src/app/apropos/page.jsx
+++ src/app/apropos/page.jsx
@@ -9,6 +9,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/virtual';
 import { Pagination } from 'swiper/modules';
+import SectionContact from "@/components/sections/SectionContact";
 
 const About = () => {
     return <>

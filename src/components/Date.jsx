551b451dd2a0097292679ff6adc47b07c5d0dc1b
massahoudou <9324@.Light>
2023-08-22 16:01:12 +0000

    changing program

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -13,7 +13,7 @@ export const Date = () => {
 
             </motion.span>
             <span className="uppercase text-sm  sm:text-md p-2 ">
-                25 au 26 Août 2023 p-2 
+                25 au 26 Août 2023 
             </span>
         </div>
         <div  data-aos="fade-up" data-aos-delay=""  className="uppercase text-sm  sm:text-md font-medium ">
